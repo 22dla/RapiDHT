25468bf1715636be4825e5f357809d0c3c428609
22dla <volkov22dla@yandex.ru>
2025-08-14 23:23:36 +0300

    вынесен конструктор и удален конструктор по умолчанию

--- include/rapidht.h
+++ include/rapidht.h
@@ -11,40 +11,9 @@ enum class Modes { CPU, GPU, RFFT };
 
 class HartleyTransform {
 public:
-	HartleyTransform(size_t width, size_t height = 0, size_t depth = 0, Modes mode = Modes::CPU) :_mode(mode) {
-		if (width <= 0 || height < 0 || depth < 0) {
-			throw std::invalid_argument(
-				"Error (initialization): at least Rows must be positive. "
-				"height and depth can be zero (by default) but not negative.");
-		}
-		if (height == 0 && depth > 0) {
-			throw std::invalid_argument(
-				"Error (initialization): if height is zero, depth must be zero.");
-		}
+	HartleyTransform() = delete;
 
-		_dims[0] = static_cast<size_t>(width);
-		_dims[1] = static_cast<size_t>(height);
-		_dims[2] = static_cast<size_t>(depth);
-
-		// Preparation to 1D transforms
-		if (_mode == Modes::CPU || _mode == Modes::RFFT) {
-			for (size_t i = 0; i < _bit_reversed_indices.size(); ++i) {
-				_bit_reversed_indices[i].resize(_dims[i]);
-				bit_reverse(&_bit_reversed_indices[i]);
-			}
-		}
-		if (_mode == Modes::GPU) {
-			// Initialize Vandermonde matrice on the host
-			initialize_kernel_host(&_h_transform_matrix_x, width);
-			//initializeKernelHost(h_A, width);
-			//initializeKernelHost(h_A, width);
-
-
-			// transfer CPU -> GPU
-			_d_transform_matrix_x.resize(_dims[0] * _dims[1]);
-			_d_transform_matrix_x.set(&_h_transform_matrix_x[0], _dims[0] * _dims[1]);
-		}
-	}
+	HartleyTransform(size_t width, size_t height, size_t depth, Modes mode);
 	void ForwardTransform(double* data);
 	void InverseTransform(double* data);
 
